08448a706e17322d7bdb1bf7d890100089041b39
Kim Gaeun <kge2015@gmail.com>
2024-01-25 03:20:58 +0900

    Initial commit

---
+++ src/main/java/com/example/chickenproject/ChickenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.chickenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chickenproject/ChickenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chickenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
