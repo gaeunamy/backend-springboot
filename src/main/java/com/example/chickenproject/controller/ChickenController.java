514ab97ee5aaa5785fa903b344c47d1147dd4a1a
Kim Gaeun <kge2015@gmail.com>
2024-01-27 22:09:58 +0900

    feat: 폼 데이터 받아 DTO에 담기

--- src/main/java/com/example/chickenproject/controller/ChickenController.java
+++ src/main/java/com/example/chickenproject/controller/ChickenController.java
@@ -1,12 +1,20 @@
 package com.example.chickenproject.controller;
 
+import com.example.chickenproject.dto.ChickenForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChickenController {
-    @GetMapping("/articles/new")
+    @GetMapping("/chickens/new")
     public String newChickenForm() {
-        return "articles/new";
+        return "chickens/new";
+    }
+
+    @PostMapping("/chickens/create")
+    public String createChicken(ChickenForm form) {
+        System.out.println(form.toString());
+        return "";
     }
 }
