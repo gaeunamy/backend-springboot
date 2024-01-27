67efea5a84282eac46e241f1adf63d12b0b8ad1e
Kim Gaeun <kge2015@gmail.com>
2024-01-27 21:48:15 +0900

    feat: 뷰 페이지 반환할 컨트롤러 생성

--- src/main/java/com/example/chickenproject/controller/ChickenController.java
+++ src/main/java/com/example/chickenproject/controller/ChickenController.java
@@ -1,7 +1,12 @@
 package com.example.chickenproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChickenController {
+    @GetMapping("/articles/new")
+    public String newChickenForm() {
+        return "articles/new";
+    }
 }
