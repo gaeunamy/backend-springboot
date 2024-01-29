3f97c798274b0141e7e43a392d4648a90cb95ce6
Kim Gaeun <kge2015@gmail.com>
2024-01-29 13:19:33 +0900

    chore: println()문 로깅으로 대체

--- src/main/java/com/example/chickenproject/controller/ChickenController.java
+++ src/main/java/com/example/chickenproject/controller/ChickenController.java
@@ -3,11 +3,13 @@ package com.example.chickenproject.controller;
 import com.example.chickenproject.dto.ChickenForm;
 import com.example.chickenproject.entity.Chicken;
 import com.example.chickenproject.repository.ChickenRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ChickenController {
     @Autowired
@@ -20,13 +22,16 @@ public class ChickenController {
 
     @PostMapping("/chickens/create")
     public String createChicken(ChickenForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        // System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Chicken chicken = form.toEntity();
-        System.out.println(chicken.toString());
+        log.info(chicken.toString());
+        // System.out.println(chicken.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Chicken saved = chickenRepository.save(chicken);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        // System.out.println(saved.toString());
         return "";
     }
 }
