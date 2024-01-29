4c2fa1f91a305ed1539d20d9e3dd944153725d29
Kim Gaeun <kge2015@gmail.com>
2024-01-29 13:18:47 +0900

    refactor: 롬복 활용해 리팩터링

--- src/main/java/com/example/chickenproject/dto/ChickenForm.java
+++ src/main/java/com/example/chickenproject/dto/ChickenForm.java
@@ -1,23 +1,14 @@
 package com.example.chickenproject.dto;
 
 import com.example.chickenproject.entity.Chicken;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ChickenForm {
     private String title;
     private String content;
-
-    public ChickenForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ChickenForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Chicken toEntity() {
         return new Chicken(null, title, content);
--- src/main/java/com/example/chickenproject/entity/Chicken.java
+++ src/main/java/com/example/chickenproject/entity/Chicken.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Chicken {
     @Id
@@ -14,19 +18,4 @@ public class Chicken {
     private String title;
     @Column
     private String content;
-
-    public Chicken(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Chicken{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
